c110146928ea0f921a95c4269f9ce2c31bbd8abd
Thapelo-ST <tsamkelo96@gmail.com>
2023-09-20 18:28:26 -0700

    user.py

--- models/user.py
+++ models/user.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.Place import place
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
     __tablename__ = 'users'
@@ -16,3 +17,4 @@ class User(BaseModel):
     last_name = Column(String(128), nullable=False)
 
     places = relationship("Place", back_populates="user")
+    
