9807ce0949a55ed4d48d078492f457f31fba3511
Thapelo-ST <tsamkelo96@gmail.com>
2023-10-05 05:48:40 -0700

    task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,42 +3,31 @@
 Fabric script to distribute an archive to your web servers.
 """
 
-from fabric.api import env, put, run, local
-from os.path import exists
+from fabric.api import *
+from os import path
 
-env.hosts = ['34.227.93.64', '3.85.54.154']
 env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
+env.hosts = ['34.227.93.64','3.85.54.154']
 
 def do_deploy(archive_path):
     """Deploys an archive to the web servers."""
-    if not exists(archive_path):
+    if not path.exists(archive_path):
         return False
 
     try:
-        put(archive_path, '/tmp/')
+        archive_name = archive_path.split("/")[-1]
+        archive_no_ext = archive_name.replace(".tgz","")
+        remote_temp = "/tmp/{}".format(archive_name)
+        remote_dest = "/data/web_static/releases/{}/".format(archive_no_ext)
 
-        archive_filename = archive_path.split('/')[-1]
-        archive_name = archive_filename.split('.')[0]
-
-        run('mkdir -p /data/web_static/releases/{}/'.format(archive_name))
-
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
-            archive_filename, archive_name))
-
-        run('rm /tmp/{}'.format(archive_filename))
-
-        run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/'.format(archive_name, archive_name))
-
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(
-            archive_name))
-
-        run('rm -rf /data/web_static/current')
-
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
-            .format(archive_name))
-
+        put(archive_path, remote_tmp)
+        run("mkdir -p {}".format(remote_dest))
+        run("tar -xzf {} -C {}".format(remote_tmp, remote_dest))
+        run("rm {}".format(remote_tmp))
+        run("mv {}web_static/* {}".format(remote_dest, remote_dest))
+        run("rm -rf {}web_static".format(remote_dest))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(remote_dest))
         print("New version deployed!")
         return True
     except Exception:
